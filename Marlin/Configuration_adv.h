8c6dd7665953c9d4c06f313b6a7607895ea63b04
Rolf Schäuble <rolf@rschaeuble.de>
2019-03-09 23:42:10 +0100

    Enable S-curve acceleration and junction deviation.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -450,9 +450,9 @@
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
-//#define JUNCTION_DEVIATION
+#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.07  // (mm) Distance from real junction edge
 #endif
 
 /**
